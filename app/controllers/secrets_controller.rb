97bbbadec112bf5ae80fdfd2e5ce3eeeb0e91d23
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-06-27 13:15:54 -0500

    styling and created_at

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,7 @@
 class SecretsController < ApplicationController
 
   def create
-      @response = Http.post("http://localhost:3000/secrets.json", json: {secret:{title: params[:secret][:title], comment: params[:secret][:comment]}})
+      @response = Http.post("http://localhost:3000/secrets.json", json: {secret:{title: params[:secret][:title], comment: params[:secret][:comment], created_at: params[:secret][:created_at]}})
   end
 
   def index
