f4b07c5d057fbb3844ffa8c9ff2d146236f87b95
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-06-27 17:56:57 -0500

    new user auto directs to secrets page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
   def create
     @response = Http.post("http://localhost:3000/users.json", json: {user:{email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confiration], name: params[:user][:name]}})
 
+
+    user = JSON.parse(@response)
+    session[:name] = user["name"]
+    session[:token] = user["token"]
+
     @name = session[:name]
   end
 
