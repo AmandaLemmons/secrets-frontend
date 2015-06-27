2bdd1e51e9c66597f59bbb761d14c6e2c6b8f5f4
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-06-27 10:28:37 -0500

    working login/signup

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,12 +8,15 @@ require 'http'
 
     @http_response = Http.post("http://localhost:3000/auth.json", json: {email: params[:email], password: params[:password]})
 
-    @user = JSON.parse(@http_response)
+    user = JSON.parse(@http_response)
+    session[:name] = user["name"]
+    session[:token] = user["token"]
+    @name = session[:name] 
 
   end
 
   def destroy
-    session[:name] = nil 
+    session[:name] = nil
     redirect_to root_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,6 @@ class UsersController < ApplicationController
   def create
     @response = Http.post("http://localhost:3000/users.json", json: {user:{email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confiration], name: params[:user][:name]}})
 
-    session[:name] = user["name"]
-    session[:token] = user["token"]
     @name = session[:name]
   end
 
